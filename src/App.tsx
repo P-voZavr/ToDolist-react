a017a916c2d648a69c12b9d3d16ba599190450ed
P_voZavr <165915639+P-voZavr@users.noreply.github.com>
2025-08-14 17:52:09 +0300

    Refactor PointerButtons

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import "./css/main.css";
 import { useState } from "react";
 import ToDoAdd from "./components/ToDoAdd/ToDoAdd";
 import ToDoTable from "./components/ToDoTable/ToDoTable.tsx";
-import PointerButtons from "./components/PointerButtons/PointerButtons.tsx";
 
 function App() {
   const [ToDolst, ToDolstChange] = useState<any[]>([]);
@@ -12,7 +11,6 @@ function App() {
       <main className="MainPage">
         <ToDoAdd ToDolst={ToDolst} ToDolstChange={ToDolstChange} />
         <ToDoTable ToDolst={ToDolst} ToDolstChange={ToDolstChange} />
-        <PointerButtons />
       </main>
     </>
   );
--- src/components/ToDoTable/ToDoTable.tsx
+++ src/components/ToDoTable/ToDoTable.tsx
@@ -1,14 +1,21 @@
 import "./style.css";
 import ToDoObj from "../TodoObj/ToDoObj";
+import PointerButtons from "../PointerButtons/PointerButtons";
+import { useState } from "react";
 type props = {
   ToDolst: string[];
   ToDolstChange: React.Dispatch<React.SetStateAction<string[]>>;
 };
 function ToDoTable({ ToDolst, ToDolstChange }: props) {
+  const [page, setpage] = useState(0);
+
+  const totalpages = Math.ceil(ToDolst.length / 5);
+
   return (
     <div className="ToDoTable">
-      {ToDolst.length > 5
-        ? [...ToDolst]
+      {ToDolst.length > 5 ? (
+        <>
+          {[...ToDolst]
             .slice(ToDolst.length - 5, ToDolst.length)
             .map((val, index) => (
               <ToDoObj
@@ -18,16 +25,20 @@ function ToDoTable({ ToDolst, ToDolstChange }: props) {
                 i={index}
                 key={index + val}
               />
-            ))
-        : ToDolst.map((val, index) => (
-            <ToDoObj
-              ToDolstChange={ToDolstChange}
-              ToDolst={ToDolst}
-              text={val}
-              i={index}
-              key={index + val}
-            />
-          ))}
+            ))}
+          <PointerButtons />
+        </>
+      ) : (
+        ToDolst.map((val, index) => (
+          <ToDoObj
+            ToDolstChange={ToDolstChange}
+            ToDolst={ToDolst}
+            text={val}
+            i={index}
+            key={index + val}
+          />
+        ))
+      )}
     </div>
   );
 }
