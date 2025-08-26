d8d51ecdb49f774b290401cf7d81698426dcf4ee
P_voZavr <165915639+P-voZavr@users.noreply.github.com>
2025-08-26 23:40:15 +0300

    important fix

--- src/help/searchlistLength.ts
+++ src/help/searchlistLength.ts
@@ -1,6 +1,7 @@
 import { useToDoStore } from "../store/useToDoStore";
 
-const { ToDolst, ToDovalue } = useToDoStore();
+const ToDolst = useToDoStore.getState().ToDolst;
+const ToDovalue = useToDoStore.getState().ToDovalue;
 
 const searchlstLength = ToDolst.map((val, index) => {
   if (val.text.includes(ToDovalue.trim())) return index;
